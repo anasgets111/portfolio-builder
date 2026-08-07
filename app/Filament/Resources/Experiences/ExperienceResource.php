776f82dbc3488f99799fe462d0cff59bcab1048c
Anas Khalifa <anasgets111@gmail.com>
2026-08-07 04:35:55 +0300

    refactor: improve admin resource icons

--- app/Filament/Resources/Experiences/ExperienceResource.php
+++ app/Filament/Resources/Experiences/ExperienceResource.php
@@ -18,7 +18,7 @@ class ExperienceResource extends Resource
 {
     protected static ?string $model = Experience::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBriefcase;
 
     protected static ?string $recordTitleAttribute = 'company';
 
